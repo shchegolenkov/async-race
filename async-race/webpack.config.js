c96210aaf109a8667783efe1626f5657ff47f7c0
shchegolenkov <denis.amorales@ya.ru>
2023-07-13 18:01:27 +0300

    init: start async-race

---
+++ async-race/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
+
+const baseConfig = {
+    entry: path.resolve(__dirname, './src/scripts/index'),
+    mode: 'development',
+    module: {
+        rules: [
+            { test: /\.ts$/i, use: 'ts-loader' },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(ico|gif|png|jpg|jpeg|svg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.html$/i,
+                use: 'html-loader',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.json'],
+        alias: {
+            types: path.resolve(__dirname, './src/types/')
+        }
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, '../dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new MiniCssExtractPlugin(),
+        new CleanWebpackPlugin(),
+        new EslingPlugin({ extensions: 'ts' })
+    ],
+};
+
+module.exports = ({ mode }) => {
+    const isProductionMode = mode === 'prod';
+    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+    return merge(baseConfig, envConfig);
+};
